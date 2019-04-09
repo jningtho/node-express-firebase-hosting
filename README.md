4669e625b6cfa5ec65c8e6f695b6f4ed1319620d
jningtho <jningtho@x-designs.net>
2019-04-09 20:25:37 +0530

    updated Templating + README

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 ## node-express-firebase-hosting
 * Templating Engine: ejs
 
+### Stack
+* NodJS
+* Express
+* EJS Templating
+* Bootstrap 4.x
+* [HTML Template](https://colorlib.com)
 
 ## TO DO
 * Add Templating
--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,10 @@
+const firebase = require('firebase');
 const functions = require('firebase-functions');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const fs = require('fs');
+// const fs = require('fs');
 const ejs = require('ejs');
-const http = require('http');
 
 const app = express();
 
@@ -21,17 +21,22 @@ app.use(express.static('public/fonts'));
 app.use(express.static('public/img'));
 app.use(express.static('public/js'));
 
-// app.engine('html', require('ejs').renderFile);
-// app.set('view engine', 'html');
-// app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine('html', ejs.renderFile);
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+
+// app.use(express.static(path.join(__dirname + 'public')));
+
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
 
 // app.use(express.static(path.join(__dirname + './public')));
 
 // ejs.registerPartial('partial', fs.readFileSync(__dirname + '/views/partials/', 'utf8'));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.render('index.ejs');
   });
 
@@ -44,14 +49,20 @@ app.get('/about', (req, res) => {
   });
 
 //404
-app.get('*', function (req, res, next) {
+app.get('*', (req, res, next) => {
     res.render('404.ejs');
   });
 
 exports.app = functions.https.onRequest(app);
 
 //initiate server
-var port = process.env.PORT || 5000;
-app.listen(port, function () {
-    console.log('Server listening on port ' + port + '...');
-  });
+// var port = process.env.PORT || 5000;
+// app.listen(port, function () {
+//     console.log('Server listening on port ' + port + '...');
+//   });
+const server = app.listen(5000, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log(`App listening at http://${host}:${port}`);
+});
